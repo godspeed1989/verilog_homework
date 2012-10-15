fe1e8dd8db612ec88dc8b6db70d5fd28863ebc1d
godspeed1989 <tongxinjichu@gmail.com>
2012-10-15 22:23:35 +0800

    readme modify

--- serial.v
+++ serial.v
@@ -1,28 +1,36 @@
+`timescale 1ns / 1ns
+/*
+ * test bench
+ */
 module serial_tb();
 	reg clk, istream;
 	reg [16:0] in;
 	wire ostream;
 	integer i;
+	parameter len = 16;
 initial begin
 	$dumpfile("serial.vcd");
 	$dumpvars();
 	clk = 0;
-	in = 17'b11011111101011110;
+	in = 16'b1101111110101110;
 	istream = 0;
-	i = 16;
+	i = 0;
 end
 always #2 clk = ~clk;
 	serial Serial(istream, ostream, clk);
 always @ (posedge clk or negedge clk)
 begin
-	if(i>=0) begin
+	if(i<len)
+	begin
 		istream <= in[i];
-		i <= i - 1;
+		i = i+1;
 	end else
 		#2 $finish();
 end
 endmodule
-
+/*
+ * serial detector
+ */
 module serial(istream, ostream, clk);
 	input istream, clk;
 	output reg ostream;
@@ -33,17 +41,17 @@ initial begin
 end
 always @ (posedge clk or negedge clk)
 begin
-	if(istream == 1'b0) begin
-		cnt <= 0;
-		ostream <= 1'b0;
-	end
-	else begin
+	if(istream == 1'b0)
+		cnt = 0;	
+	else
 		cnt = cnt + 1;
-		if(cnt == 3'b100) begin
-			cnt <= 3'b11;
-			ostream <= 1'b1;
-		end
-	end
-	$display("%b", ostream);
+
+	if(cnt == 3'b100)
+	begin
+		ostream = 1'b1;
+		cnt = 3'b011;
+	end else
+		ostream = 1'b0;
+	$display("%d %b", cnt, ostream);
 end
 endmodule
